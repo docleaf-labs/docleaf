f41d64952cc46b2b77de3379b6589cc17032b3c6
Michael Jones <m.pricejones@gmail.com>
2023-03-10 13:45:36 +0000

    Add groups examples and docs
    This should have been included with the previous groups commit. I
    failed to add these new files.

---
+++ documentation/source/groups.rst
@@ -0,0 +1,22 @@
+
+Groups
+======
+
+Group 1
+-------
+
+.. doxygengroup:: group1
+   :project: groups
+
+Group 2
+-------
+
+.. doxygengroup:: group2
+   :project: groups
+
+Group 3
+-------
+
+.. doxygengroup:: group3
+   :project: groups
+
---
+++ examples/groups/example.h
@@ -0,0 +1,36 @@
+/*! @defgroup group1 The First Group
+    @defgroup group2 The Second Group
+    @defgroup group3 The Third Group
+*/
+
+/*! @ingroup group1
+  A function
+
+  \param arg1 the first arg
+  \param arg2 the second arg
+*/
+void example_function(int arg1, bool arg2);
+
+/*! @ingroup group2
+  Another function
+
+  \param arg1 the first arg
+  \param arg2 the second arg
+*/
+void another_example_function(int arg1, bool arg2);
+
+/*! @ingroup group2
+  A third function
+
+  \param arg1 the first arg
+  \param arg2 the second arg
+*/
+void a_third_example_function(int arg1, bool arg2);
+
+/*! @ingroup group3
+  A fourth function
+
+  \param arg1 the first arg
+  \param arg2 the second arg
+*/
+void a_fourth_example_function(int arg1, bool arg2);
