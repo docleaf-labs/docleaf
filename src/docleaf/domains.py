3942327fc774159275a1361e62bd684c1f881afb
Michael Jones <m.pricejones@gmail.com>
2023-05-20 16:11:53 +0100

    Refactor domain code to reduce duplication
    It was obviously getting problematic and overdue.

--- src/docleaf/domains.py
+++ src/docleaf/domains.py
@@ -4,92 +4,56 @@ from sphinx.domains import cpp
 from docutils import nodes
 
 from . import copied
+from .errors import DocleafError
 
+null_handler = lambda finder: finder
+
+def enumerator_handler(finder):
+    # We pass EnumName::EnumeratorName to the CPPEnumeratorObject directive but we don't want to have the
+    # "EnumName::" part in the output so we find the 'desc_addname' that Sphinx uses for it and remove it
+    finder.declarator.children = [node for node in finder.declarator.children if node.tagname != "desc_addname"]
+
+cpp_domain = {
+    "class": (cpp.CPPClassObject, null_handler),
+    "enum": (cpp.CPPEnumObject, null_handler),
+    "enumerator": (cpp.CPPEnumeratorObject, enumerator_handler),
+    "function": (cpp.CPPFunctionObject, null_handler),
+    "struct": (cpp.CPPClassObject, null_handler),
+}
+
+domains = {
+    "cpp": cpp_domain
+}
 
 def render_domain_entry(name: str, type: str, declaration: str, target, directive_args: list, content: list):
-    directive_name = f"{name}:{type}"
     # print("render_domain_entry", name, type, declaration)
-    if name == "cpp" and type == "function":
-        args = [directive_name, [declaration]] + directive_args[2:]
-        directive = cpp.CPPFunctionObject(*args)
+    try:
+        domain = domains[name]
+    except KeyError:
+        raise DocleafError(f"Unsupported domain: {name}")
 
-        nodes = directive.run()
+    try:
+        (Directive, handler) = domain[type]
+    except KeyError:
+        raise DocleafError(f"Unsupported type \"{type}\" on domain \"{name}\"")
 
-        rst_node = nodes[1]
-        finder = copied.NodeFinder(rst_node.document)
-        rst_node.walk(finder)
+    directive_name = f"{name}:{type}"
 
-        set_children(finder.content, content)
-        finder.declarator.children.insert(0, target)
+    args = [directive_name, [declaration]] + directive_args[2:]
+    directive = Directive(*args)
 
-        return nodes
+    nodes = directive.run()
 
-    elif name == "cpp" and type == "class":
-        args = [directive_name, [declaration]] + directive_args[2:]
-        directive = cpp.CPPClassObject(*args)
+    rst_node = nodes[1]
+    finder = copied.NodeFinder(rst_node.document)
+    rst_node.walk(finder)
 
-        nodes = directive.run()
+    set_children(finder.content, content)
+    finder.declarator.children.insert(0, target)
 
-        rst_node = nodes[1]
-        finder = copied.NodeFinder(rst_node.document)
-        rst_node.walk(finder)
+    handler(finder)
 
-        set_children(finder.content, content)
-        finder.declarator.children.insert(0, target)
-
-        return nodes
-
-    elif name == "cpp" and type == "struct":
-        args = [directive_name, [declaration]] + directive_args[2:]
-        directive = cpp.CPPClassObject(*args)
-
-        nodes = directive.run()
-
-        rst_node = nodes[1]
-        finder = copied.NodeFinder(rst_node.document)
-        rst_node.walk(finder)
-
-        set_children(finder.content, content)
-        finder.declarator.children.insert(0, target)
-
-        return nodes
-
-    elif name == "cpp" and type == "enum":
-        args = [directive_name, [declaration]] + directive_args[2:]
-        directive = cpp.CPPEnumObject(*args)
-
-        nodes = directive.run()
-
-        rst_node = nodes[1]
-        finder = copied.NodeFinder(rst_node.document)
-        rst_node.walk(finder)
-
-        set_children(finder.content, content)
-        finder.declarator.children.insert(0, target)
-
-        return nodes
-
-    elif name == "cpp" and type == "enumerator":
-        args = [directive_name, [declaration]] + directive_args[2:]
-        directive = cpp.CPPEnumeratorObject(*args)
-
-        nodes = directive.run()
-
-        rst_node = nodes[1]
-        finder = copied.NodeFinder(rst_node.document)
-        rst_node.walk(finder)
-
-        set_children(finder.content, content)
-        finder.declarator.children.insert(0, target)
-
-        # We pass EnumName::EnumeratorName to the CPPEnumeratorObject directive but we don't want to have the
-        # "EnumName::" part in the output so we find the 'desc_addname' that Sphinx uses for it and remove it
-        finder.declarator.children = [node for node in finder.declarator.children if node.tagname != "desc_addname"]
-
-        return nodes
-
-    else:
-        raise Exception(f"Unsupported domain name ({name}) and type ({type})")
+    return nodes
 
 
 def set_children(node, children):
